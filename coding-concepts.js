93a17cd7b548265760d31488588476ed268df179
OakleyJacob <jboyoakley@gmail.com>
2023-05-19 16:37:41 -0500

    Jacob Oakley's week 2 assessment

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,36 +8,39 @@
 // --------------------1) What will this log?
 
 const cohort = "Delta 2023"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:["D", "e", "l", "t", "a", "2", "0", "2", "3"]
+// b) Verify and explain: ['D', 'e', 'l', 't', 'a', ' ', '2' , '0', '2', '3]
+// The .split method will split a string up at a specific delimiter, if an empty string is used as the delimiter it splits the string up by character
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:'Hello, Learn Studen"
+// b) Verify and explain: definitely logged undefined, the function is missing its return statement
+// tricksy hobbitses
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
-// console.log(multipliedByTwo)
+console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[8, 10, 12, 14, 16]
+// b) Verify and explain: .map is a higher order function that applies any function passed as an argument to the individual elements of the array it was called on
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[11, 13, 15]
+// b) Verify and explain:.filter is an HOF that returns a smaller array than the original, and has a built in conditional. It essentially says, pass this value into the new array IF this thing is true.
+
 
 // --------------------5) What will this log?
 
@@ -47,10 +50,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 'javascript'
+// b) Verify and explain:object and dot notation dictate that to reach elements inside javascript objects you use the . operator, and to access an element of an array(even an embedded one) we use bracket notation. 
 
 // --------------------STRETCH: What will this log?
 
@@ -62,7 +65,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: {student: George, cohort: "Delta", year: 2023}
+// b) Verify and explain: the method on 67 creates a new student named george based on the class above, the class takes in the name argument and applies it to the student field while the other variables that are static
